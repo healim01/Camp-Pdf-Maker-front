5321a6df7b941c7e836424dc89c19ceb64347869
healim01 <22000770@handong.edu>
2023-08-28 00:02:30 +0900

    :sparkles: pdf maker

--- src/components/PdfMaker.tsx
+++ src/components/PdfMaker.tsx
@@ -8,10 +8,23 @@ import {
   Font,
   Image,
 } from '@react-pdf/renderer';
-import { PDFViewer } from '@react-pdf/renderer';
 import myfont from '../fonts/KimjungchulMyungjo-Bold.ttf';
 
-export default function PdfMaker({ campInfo }: { campInfo: any }) {
+interface IcampInfo {
+  campID: number;
+  campName: string;
+  campImg: string;
+  campSeason: string;
+  campProf: string;
+  student: Istudent[];
+}
+
+interface Istudent {
+  studentID: number;
+  studentName: string;
+}
+
+export default function PdfMaker({ campInfo }: { campInfo: IcampInfo }) {
   console.log('camp pdf Maker');
   Font.register({
     family: 'KoreanFont',
@@ -21,48 +34,48 @@ export default function PdfMaker({ campInfo }: { campInfo: any }) {
   const styles = StyleSheet.create({
     page: {
       flexDirection: 'row',
+      backgroundColor: '#E4E4E4',
       fontFamily: 'KoreanFont',
     },
     section: {
-      position: 'absolute',
-      top: 100,
-      left: 100,
+      margin: 10,
+      padding: 100,
+      flexGrow: 1,
+      // position: 'absolute',
+      // top: 100,
+      // left: 100,
     },
     text: {
       fontSize: 14,
-    },
-    title: {
-      fontSize: 45,
-      fontWeight: 'bold',
     },
     backgroundImage: {
       position: 'absolute',
       width: '100%',
       height: '100%',
-      zIndex: -1,
     },
     contentWrapper: {
       position: 'relative',
       flex: 1,
     },
+    title: {
+      fontSize: 40,
+    },
   });
 
   return (
-    <PDFViewer width="1000" height="600">
-      <Document>
-        <Page size="A4" style={styles.page}>
-          <View style={styles.contentWrapper}>
-            <Image src="/pdfCover.png" style={styles.backgroundImage} />
-            <View style={styles.section}>
-              <Text>
-                <Text style={styles.title}> 수료증 </Text>
-                이름: {campInfo.campName} {'\n'}
-                학번: {campInfo.campSeason} {'\n'}
-              </Text>
-            </View>
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={styles.contentWrapper}>
+          <Image src="/pdfCover.png" style={styles.backgroundImage} />
+          <View style={styles.section}>
+            <Text style={styles.title}>수료증 {'\n'}</Text>
+            <Text>
+              이름: {campInfo.campName} {'\n'}
+              학번: {campInfo.campSeason} {'\n'}
+            </Text>
           </View>
-        </Page>
-      </Document>
-    </PDFViewer>
+        </View>
+      </Page>
+    </Document>
   );
 }
