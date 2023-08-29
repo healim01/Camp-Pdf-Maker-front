b67a9c9e570edba452273054b6448bb7c34f67c0
healim01 <22000770@handong.edu>
2023-08-29 10:39:20 +0900

    :lipstick: style: add theme

--- src/theme.ts
+++ src/theme.ts
@@ -4,11 +4,8 @@ declare module '@mui/material/styles' {
   interface Palette {
     color: {
       main: string;
-      green2: string;
-      green3: string;
-      green4: string;
-      sub: string;
-      blue2: string;
+      mint: string;
+      darkblue: string;
     };
     mono: {
       white: string;
@@ -29,12 +26,9 @@ declare module '@mui/material/styles' {
 const theme = createTheme({
   palette: {
     color: {
-      main: '#2CE477',
-      green2: '#2ACF6C',
-      green3: '#C5DF9D',
-      green4: '#EAFCF1',
-      sub: '#71CBFD',
-      blue2: '#C8EBFE',
+      main: '#3964c3',
+      mint: '#7ed8b8',
+      darkblue: '#2e4b90',
     },
     mono: {
       black: '#1A1C19',
