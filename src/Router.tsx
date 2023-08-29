b85ceaee1451063e28bf8732382671d1c12420d8
healim01 <22000770@handong.edu>
2023-08-29 10:47:12 +0900

    :sparkles: feat: add routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 // import { useRecoilValue } from 'recoil';
 import Main from './routes/Main';
 import Camp from './routes/Camp';
+import Home from './routes/Home';
+import Addinfo from './routes/AddInfo';
+import MyPage from './routes/MyPage';
 
 // import { IsLoginState } from './store/atom';
 
@@ -16,10 +19,19 @@ function Router() {
         {/* {isUserLoggedIn ? ( */}
         <>
           <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/main">
             <Main />
           </Route>
           <Route exact path="/camp/:campId">
             <Camp />
+          </Route>
+          <Route exact path="/addInfo">
+            <Addinfo />
+          </Route>
+          <Route exact path="/mypage">
+            <MyPage />
           </Route>
         </>
         {/* ) : (
