76d534e6b6d2ee0fc5993de7771a3868dd1df360
healim01 <22000770@handong.edu>
2023-08-29 10:45:00 +0900

    :lipstick: change color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,21 @@
 import styled from 'styled-components';
 
-const Container = styled.footer`
+const Div = styled.footer`
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100px;
   width: 100%;
-  background-color: lightyellow;
+  background-color: black;
+  color: white;
   position: absolute;
   bottom: 0;
 `;
 
 export default function Footer() {
   return (
-    <Container>
+    <Div>
       <p>Copyright &copy; 2023 WALAB Hyelim Choi. All rights reserved.</p>
-    </Container>
+    </Div>
   );
 }
