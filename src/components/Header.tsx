cc1d865fc3adf5982baa9c54b49241304ee0e513
healim01 <22000770@handong.edu>
2023-08-29 10:44:07 +0900

    :lipstick: style: change Img, add css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,18 @@ import styled from 'styled-components';
 import GoogleButton from '../auth/GoogleButton';
 import { useRecoilValue } from 'recoil';
 import { IsLoginState } from '../store/atom';
+import logo from '../imgs/Logo.png';
+import { Link } from 'react-router-dom';
+import { HiHome } from 'react-icons/hi';
+import theme from '../theme';
 
-const Container = styled.header`
+const Div = styled.header`
   height: 120px;
-  /* background-color: lightblue; */
-  margin-bottom: 30px;
+  width: 100%;
   display: flex;
   align-items: center;
   justify-content: space-around;
+  border-bottom: 3px solid ${theme.palette.color.main};
 `;
 
 const Logo = styled.img`
@@ -24,26 +28,41 @@ const MENU = styled.div`
 `;
 const Item = styled.div`
   width: 150px;
-  height: 40px;
-  border: 1px solid black;
+  /* height: 40px; */
+  /* border: 1px solid black; */
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: baseline;
+  gap: 5px;
+
+  &:hover {
+    border: 3px solid ${theme.palette.color.mint};
+    border-radius: 50px;
+    padding: 10px 30px;
+  }
 `;
 
 export default function Header() {
   const isUserLoggedIn = useRecoilValue(IsLoginState);
   return (
     <>
-      <Container>
-        <Logo src="https://csee.handong.edu/wp-content/uploads/2018/02/csee-logo-symbol-e1518540168998.png" />
+      <Div>
+        <Link to="/main">
+          <Logo src={logo} />
+        </Link>
         <div />
         <MENU>
-          {/* <Item> Camp 모두 보기 </Item>
-          <Item> Login </Item> */}
-          {isUserLoggedIn ? <Item> MY PAGE </Item> : <GoogleButton />}
+          {isUserLoggedIn ? (
+            <Link to="/mypage">
+              <Item>
+                <HiHome /> MY PAGE
+              </Item>
+            </Link>
+          ) : (
+            <GoogleButton />
+          )}
         </MENU>
-      </Container>
+      </Div>
     </>
   );
 }
