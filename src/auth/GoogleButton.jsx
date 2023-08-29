ec33124d85f92798c208fb44f545fa1eb977a40e
healim01 <22000770@handong.edu>
2023-08-29 10:40:23 +0900

    :fire: remove comments

--- src/auth/GoogleButton.jsx
+++ src/auth/GoogleButton.jsx
@@ -2,51 +2,22 @@
 import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
 import jwtDecode from 'jwt-decode';
 import { useHistory } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
-import { IsLoginState, MemberIdState } from '../store/atom';
-import axios from 'axios';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { IsLoginState, UserEmail } from '../store/atom';
 
 export default function GoogleButton() {
   // eslint-disable-next-line no-unused-vars
   const [Login, setLogin] = useRecoilState(IsLoginState);
-  // eslint-disable-next-line no-unused-vars
-  const [memberId, setMemberId] = useRecoilState(MemberIdState);
-
-  // setLogin(true);
+  const setUserEmail = useSetRecoilState(UserEmail);
 
   let history = useHistory();
 
   const onSuccess = async (credentialResponse) => {
     const decodedToken = jwtDecode(credentialResponse.credential);
     console.log(decodedToken);
-
-    try {
-      // const response = await axios.post(
-      //   `${process.env.REACT_APP_BASE_URL}/members/google-login`,
-      //   {
-      //     googleAccountId: decodedToken.email, // Google 계정 ID
-      //     imgUrl: decodedToken.picture, // 유저 이미지 URL
-      //     code: decodedToken.sub, // Google OAuth 코드
-      //   },
-      // );
-      // if (response.status === 200) {
-      //   const memberId = response.data.memberId; // memberId 받아오기
-      //   console.log('Member ID:', memberId);
-      //   setLogin(true);
-      //   setMemberId(memberId);
-      //   history.push('/first/signUp/addName');
-      //   // memberId를 다음 단계로 넘기거나 원하는 대로 활용할 수 있습니다.
-      // } else {
-      //   throw new Error('API request failed');
-      // }
-    } catch (error) {
-      console.error('Error during login:', error);
-    }
-
+    setUserEmail(decodedToken.email);
     setLogin(true);
-    setMemberId(1);
-    console.log('Member ID:', memberId);
-    history.push('/');
+    history.push('/addInfo');
   };
 
   return (
